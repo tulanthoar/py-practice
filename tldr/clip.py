56782c426b23ee52cc1a2d526f362143e682c60d
Nate Yonkee <tulanthoar@gmail.com>
2016-04-24 13:03:44 -0600

    clean up extra stuff

--- tldr/clip.py
+++
@@ -1,3 +0,0 @@
-from pyperclip import copy,paste,set_clipboard
-set_clipboard('xsel')
-copy('hello')`
--- tldr/press_return.py
+++
@@ -1,4 +0,0 @@
-#!/bin/env python3
-from pykeyboard import PyKeyboard as PyK
-k = PyK()
-k.tap_key(k.return_key)
